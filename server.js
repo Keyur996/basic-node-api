521c84be0489500a7ad476ccfbf97bac5be7b331
Keyur996 <keyurmachhi123@gmail.com>
2021-07-16 17:02:22 +0530

    api documentation

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotEnv = require('dotenv');
 const cors = require('cors');
+const morgan = require('morgan');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml');
@@ -19,31 +20,32 @@ app.use(cors());
 // request payload middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
 
 app.use('/api/v1/product', require('./routes/productRoutes'));
 app.use('/api/v1/user', require('./routes/userRoutes'));
 
 // API Documentation
 if (process.env.NODE_ENV != 'production') {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
 
 app.get('/', (req, res, next) => {
-  res.send('Hello from Node API Server');
+    res.send('Hello from Node API Server');
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
 
 // error handler middleware
-app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send({
-    status: 500,
-    message: err.message,
-    body: {}
-  });
+app.use(function(err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send({
+        status: 500,
+        message: err.message,
+        body: {}
+    });
 })
